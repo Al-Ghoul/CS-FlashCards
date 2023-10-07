7698a75202d4583f21a9c0238dcbc0db01a76c85
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-10-07 13:12:48 +0300

    Fix slug mismatch

--- app.config.ts
+++ app.config.ts
@@ -3,9 +3,14 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  slug: "CS-FC",
+  slug: "CS-FlashCards",
   name: "CS-FlashCards",
   android: {
     googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    adaptiveIcon: {
+      foregroundImage: "./assets/splash/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    package: "com.alghoul.csflashcards",
   },
 });
