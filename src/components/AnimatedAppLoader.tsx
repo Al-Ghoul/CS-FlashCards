aa4c1cc86765897a152dd969f65b8bdd22095ddc
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-20 12:33:36 +0300

    refactor: remove useless state on app load

--- src/components/AnimatedAppLoader.tsx
+++ src/components/AnimatedAppLoader.tsx
@@ -1,18 +1,6 @@
-import { useEffect, useState } from "react";
 import AnimatedSplashScreen from "./AnimatedSplashScreen";
 
-
 export default function AnimatedAppLoader({ children }: Props) {
-  const [isSplashReady, setSplashReady] = useState(false);
-
-  useEffect(() => {
-    setSplashReady(true);
-  }, []);
-
-  if (!isSplashReady) {
-    return null;
-  }
-
   return (
     <AnimatedSplashScreen>
       {children}
@@ -21,5 +9,5 @@ export default function AnimatedAppLoader({ children }: Props) {
 }
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
